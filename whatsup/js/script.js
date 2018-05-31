2e0fa4279f9a3733ef3813e64744fecf968964db
Cameron mitchell Shaw <845872712@bun2-206-14.local>
2018-05-31 17:30:20 +1200

    navbar

--- whatsup/js/script.js
+++ whatsup/js/script.js
@@ -1,7 +1,32 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-}
+(function (){
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
+	var hamburger = document.getElementById("hamburger");
+	var closeHamburger = document.getElementById("closebtn");
+	
+
+	function openNav() {
+	    document.getElementById("mySidenav").style.width = "50%";
+	}
+
+	function closeNav() {
+	    document.getElementById("mySidenav").style.width = "0";
+	}
+
+	hamburger.addEventListener("click", function(){
+	    openNav();
+	});
+
+	closeHamburger.addEventListener("click", function(){
+	    closeNav();
+	});
+
+	// document.querySelector('.button').onmousemove = (e) => {
+	//   const x = e.pageX - e.target.offsetLeft
+	//   const y = e.pageY - e.target.offsetTop
+	//   e.target.style.setProperty('--x', `${ x }px`)
+	//   e.target.style.setProperty('--y', `${ y }px`)
+	 
+	// }	
+
+
+})();
